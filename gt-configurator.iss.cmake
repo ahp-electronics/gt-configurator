34da7473a8e409ccc77399b5e906bf35ef736227
git user at openvlbi.com <git@openvlbi.com>
2025-01-10 18:24:04 +0000

    Updated gt-configurator.iss.cmake

--- gt-configurator.iss.cmake
+++ gt-configurator.iss.cmake
@@ -1,10 +1,10 @@
-#define MyAppName "GT1 Configurator"
+#define MyAppName "GT Configurator"
 #define TargetName "gt-configurator"
 
 [Setup]
 AppId={{9C18C97C-463A-42AD-B5D7-4154023BDE23}
 AppName={#MyAppName}
-AppVersion=@GT_CONFIGURATOR_VERSION@
+AppVersion=@AHP_GT_CONFIGURATOR_VERSION@
 DefaultDirName={autopf}\AHP\{#MyAppName}
 DefaultGroupName=AHP
 UninstallDisplayIcon={app}\{#TargetName}.exe
